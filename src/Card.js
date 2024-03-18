7d77b9c87c53ba6281a4a4b1aab0e74d102a70a1
kyota-hoshi <k-hoshi@kyotakotech.blog>
2024-03-19 00:57:49 +0900

    Reactの基礎 その４：React hooks その１ useState()

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,39 @@
 import React from "react";
 
+const { useState } = React;
+
 function Card(props) {
+  const [todoStatus, setTodoStatus] = useState('Not Ready'); 
+  
+  const toggleStatus = () => {
+    switch(todoStatus) {
+      case 'Not Ready':
+        setTodoStatus('Ready');
+        break;
+      case 'Ready':
+        setTodoStatus('Compleated');
+        break;
+      case 'Compleated':
+        setTodoStatus('Not Ready')
+        break;
+      default:
+        break;
+    }
+  }
+  
   return (
     <div className='card'>
-      <input className='todocheck' type='checkbox' />
-      <p className='todocontent'>{props.content}</p>
+      <div className="todoIcon">✔︎</div>
+      <div className='todocontent'>{props.content}</div>
+      <div className="todoStatus">
+        <span>status:</span>
+        <button
+          className="statusButton"
+          onClick={() => toggleStatus()}
+        >
+          {todoStatus}
+        </button>
+      </div>
     </div>
   )
 }
