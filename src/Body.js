90d54654b2e8ade2de8cae446ac4db7770dd7b0f
kyota-hoshi <k-hoshi@kyotakotech.blog>
2024-03-22 01:04:19 +0900

    Reactの基礎 その5まで

--- src/Body.js
+++ src/Body.js
@@ -1,12 +1,75 @@
 import React from "react";
 import Card from "./Card";
 
+const { useState, useEffect } = React;
+
 function Body() {
+  const initialCards = [
+    {
+      id: '1',
+      content: 'Study English',
+      status: 'Not Ready',
+    },
+    {
+      id: '2',
+      content: 'Study React',
+      status: 'Ready',
+    },
+    {
+      id: '3',
+      content: 'Clean Room',
+      status: 'Compleated',
+    },
+  ]
+
+  const fetchCards = () => {
+    return initialCards;
+  };
+
+  const toggleStatus = (status) => {
+    switch(status) {
+      case 'Not Ready':
+        return 'Ready';
+      case 'Ready':
+        return 'Compleated';
+      case 'Compleated':
+        return 'Not Ready';
+      default:
+        throw new Error('無効なステータスです');
+    }
+  }
+
+  const handleToggleStatus = (id) => {
+    const newCards = cards.map(c => {
+      if (c.id === id) {
+        return {
+          ...c,
+          status: toggleStatus(c.status),
+        };
+      } else {
+        return c;
+      }
+    });
+    setCards(newCards);
+  }
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const cards = fetchCards();
+    setCards(cards);
+  }, []);
+
   return (
     <div className='body'>
-      <Card content='Study English' />
-      <Card content='Study React' />
-      <Card content='Clean Room' />
+      {cards.map(c => {
+        return (
+          <Card
+            card={c}
+            toggleStatus={handleToggleStatus}
+          />
+        )
+      })}
     </div>
   )
 }
--- src/Card.js
+++ src/Card.js
@@ -1,37 +1,17 @@
 import React from "react";
 
-const { useState } = React;
-
-function Card(props) {
-  const [todoStatus, setTodoStatus] = useState('Not Ready'); 
-  
-  const toggleStatus = () => {
-    switch(todoStatus) {
-      case 'Not Ready':
-        setTodoStatus('Ready');
-        break;
-      case 'Ready':
-        setTodoStatus('Compleated');
-        break;
-      case 'Compleated':
-        setTodoStatus('Not Ready')
-        break;
-      default:
-        break;
-    }
-  }
-  
+function Card({ card, toggleStatus }) {
   return (
     <div className='card'>
       <div className="todoIcon">✔︎</div>
-      <div className='todocontent'>{props.content}</div>
+      <div className='todocontent'>{card.content}</div>
       <div className="todoStatus">
         <span>status:</span>
         <button
           className="statusButton"
-          onClick={() => toggleStatus()}
+          onClick={() => toggleStatus(card.id)}
         >
-          {todoStatus}
+          {card.status}
         </button>
       </div>
     </div>
